269e603c4c1ff87f43d987252150965c682775d3
shivani <shivaniaag1247@gmail.com>
2019-01-04 01:02:59 +0530

    forecasting method

--- Arima.py
+++ Arima.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 import numpy as np
 df = pd.read_csv('train.csv', nrows = 3096)
-
-#Creating train and test set
-
-train=df[1028:]
-test=df[1:1028]
-
-import statsmodels.api as sm
+train=df[1:1028]
+test=df[1028:1200]
 y_hat_avg = test.copy()
 fit1 = sm.tsa.statespace.SARIMAX(train.Value, order=(2, 1, 4),seasonal_order=(0,1,1,7)).fit()
-y_hat_avg['SARIMA'] = fit1.predict(start="2015-01-02", end="2003-01-02", dynamic=True)
+y_hat_avg['SARIMA'] = fit1.predict(start=1027, end=1200, dynamic=True)
 plt.figure(figsize=(16,8))
 plt.plot( train['Value'], label='Train')
 plt.plot(test['Value'], label='Test')
 plt.plot(y_hat_avg['SARIMA'], label='SARIMA')
 plt.legend(loc='best')
 plt.show()
+print(test['Value'])
+print(y_hat_avg['SARIMA'])
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+rms = sqrt(mean_squared_error(test.Value, y_hat_avg.SARIMA))
+print(rms)
--- holt.py
+++ holt.py
@@ -11,7 +11,7 @@ test=df[1:1028]
 from statsmodels.tsa.api import Holt
 
 import statsmodels.api as sm
-sm.tsa.seasonal_decompose(train.Value).plot()
+sm.tsa.seasonal_decompose(train.Value, freq=3).plot()
 result = sm.tsa.stattools.adfuller(train.Value)
 plt.show()
 
@@ -26,3 +26,8 @@ plt.plot(test['Value'], label='Test')
 plt.plot(y_hat_avg['Holt_linear'], label='Holt_linear')
 plt.legend(loc='best')
 plt.show()
+
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+rms = sqrt(mean_squared_error(test.Value, y_hat_avg.Holt_linear))
+print(rms)
